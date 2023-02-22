96d6bcf65e41475e1683e7c13ad5c77262c31484
Codingwithcn <67172682+codingwithcn@users.noreply.github.com>
2023-02-22 18:28:17 -0500

    fixing previous build error

--- app.config.js
+++ app.config.js
@@ -20,7 +20,7 @@ export default {
     "ios": {
       "supportsTablet": true,
       "bundleIdentifier": "com.lifters.international.lifters",
-      "buildNumber": "17.0.0"
+      "buildNumber": "18.0.0"
     },
     "android": {
       "adaptiveIcon": {
@@ -28,7 +28,7 @@ export default {
         "backgroundColor": "#000000"
       },
       "package": "com.lifters.international.lifter",
-      "versionCode": 17,
+      "versionCode": 18,
       "permissions": [
         "android.permission.RECORD_AUDIO"
       ],
